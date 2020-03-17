d25647d787047983883daba20bf9a99917425409
Benjamin Jochum <uzebb@student.kit.edu>
2020-03-17 13:53:35 +0100

    Fix max_indices in fragment result

--- src/experiments/model/FragmentResult.py
+++ src/experiments/model/FragmentResult.py
@@ -122,19 +122,19 @@ class FragmentResult:
                                                      experiment_dataset.y_name, max_box_idx=len(self.box_results_train))
 
         self.leftmost_box_idx = -1
-        self.highest_mean_idx = int(np.argmax(list(map(lambda b: b.get_box_mean(), self.box_results_train))))
-        self.highest_f1_idx = int(np.argmax(list(map(lambda b: b.get_f1(), self.box_results_train))))
-        self.highest_f2_idx = int(np.argmax(list(map(lambda b: b.get_f2(), self.box_results_train))))
-        self.highest_wracc_idx = int(np.argmax(list(map(lambda b: b.getWRacc(), self.box_results_train))))
+        self.highest_mean_idx = self._get_box_max_box_idx(self.box_results_train, DENSITY_KEY)
+        self.highest_f1_idx = self._get_box_max_box_idx(self.box_results_train, F1_KEY)
+        self.highest_f2_idx = self._get_box_max_box_idx(self.box_results_train, F2_KEY)
+        self.highest_wracc_idx = self._get_box_max_box_idx(self.box_results_train, WRACC_KEY)
 
         self.min_mass_box = (self.boxes[self.leftmost_box_idx], self.box_results_test[self.leftmost_box_idx].get_box_mass())
         self.highest_mean_box = (self.boxes[self.highest_mean_idx], self.box_results_test[self.highest_mean_idx].get_box_mean())
         self.highest_f1_box = (self.boxes[self.highest_f1_idx], self.box_results_test[self.highest_f1_idx].get_f1())
         self.highest_f2_box = (self.boxes[self.highest_f2_idx], self.box_results_test[self.highest_f2_idx].get_f2())
         self.highest_wracc_box = (self.boxes[self.highest_wracc_idx], self.box_results_test[self.highest_wracc_idx].getWRacc())
-        self.kpis = self.get_kpi_list(self.box_results_test)
+        self.kpis = list(map(lambda box: box.get_kpis(), self.box_results_test))
 
-    def to_restriction(self, box_idx, train=True):
+    def to_restriction(self, box_idx: int, train=True) -> pd.DataFrame:
         new_restriction = self.initial_restrictions_test if train else self.initial_restrictions_test
         box = self.boxes[box_idx][0] if train else self.boxes[box_idx][0]
         for key in box:
@@ -142,17 +142,30 @@ class FragmentResult:
             new_restriction.loc[1, key] = box[key][1]
         return new_restriction
 
-    def get_box_results(self, data: pd.DataFrame, restrictions: List[pd.DataFrame], f_size: int, y_name: str, max_box_idx: int = None):
+    def get_box_results(self, data: pd.DataFrame, restrictions: List[pd.DataFrame], f_size: int, y_name: str, max_box_idx: int = None) -> List[BoxResult]:
+        """
+        Builds a list of box results. The box results contain the quality measures of each box based on the input data. The list only contains boxes
+        that have a minimum box mass. The box mass is specified in self.min_support. The f_size is necessary to ensure relative box masses.
+        :param data: Data to evaluate boxes on. This includes the response column.
+        :param restrictions: The restrtictions indicating the boxes
+        :param f_size: the size of the fragments. this is important for finding the min_mass of a box
+        :param y_name: The name of the response column
+        :param max_box_idx: Providing a value disables checking the minimum mass of a box, instead it uses a fixed maximum of the box idx
+        :return:
+        """
         box_set = []
         initial_restrictions = get_initial_restrictions(data.drop(columns=[y_name]))
-        r = max_box_idx if max_box_idx is not None else range(len(restrictions))
-        for idx in r:
+        r = max_box_idx if max_box_idx is not None else len(restrictions)
+        for idx in range(r):
             restricted_dims = d_u._determine_restricted_dims(restrictions[idx], initial_restrictions)
             br = BoxResult(data, restrictions[idx][restricted_dims], y_name)
             box_set.append(br)
-            if not self.__has_min_box_mass(br.get_box_mass(), f_size, self.min_support):
+            if not self.__has_min_box_mass(br.get_box_mass(), f_size, self.min_support) and max_box_idx is None:
                 break
         return box_set
+
+    def _get_box_max_box_idx(self, box_results_list: List[BoxResult], metric_key: str) -> int:
+        return int(np.argmax(list(map(lambda b: b.get_kpis()[metric_key], box_results_list))))
 
     def __has_min_box_mass(self, relative_box_mass: float, fragment_size: int, min_support: float) -> bool:
         if min_support >= 1:
@@ -160,6 +173,3 @@ class FragmentResult:
         else:
             mass = relative_box_mass
         return mass >= min_support and mass > 0
-
-    def get_kpi_list(self, results):
-        return list(map(lambda box: box.get_kpis(), results))
