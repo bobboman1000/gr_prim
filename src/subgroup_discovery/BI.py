fd0a1b41111b070778ad861ba2b9a1a2e976e692
Benjamin Jochum <uzebb@student.kit.edu>
2020-04-08 15:26:47 +0200

    Cleanup BI file

--- src/subgroup_discovery/BI.py
+++ src/subgroup_discovery/BI.py
@@ -1,10 +1,6 @@
 from typing import Type, List
 
 import numpy as np
-import pandas as pd
-from ema_workbench.analysis import prim as p, prim_util as pu
-
-import math
 
 import pandas as pd
 from rpy2.robjects import pandas2ri  # install any dependency package if you get error like "module not found"
@@ -23,9 +19,9 @@ class BestInterval:
         self.beam_size = beam_size
 
     def find(self, X: pd.DataFrame, y: np.ndarray, regression=True) -> List[pd.DataFrame]:
-        sdmap = SignatureTranslatedAnonymousPackage(self.get_rstring(), "sdmap")
+        bi = SignatureTranslatedAnonymousPackage(self.get_rstring(), "bi")
         X_cols = X.columns
-        result = sdmap.beam_refine(X, y, beam_size=self.beam_size)
+        result = bi.beam_refine(X, y, beam_size=self.beam_size)
         result = pd.DataFrame(result, columns=X_cols)
         return [get_initial_restrictions(X), result]
 
