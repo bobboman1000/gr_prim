da83124f8f3734976dbf3cc03947974804124808
Benjamin Jochum <uzebb@student.kit.edu>
2020-04-10 20:47:49 +0200

    move test_sd

--- test_scripts/test_sd.py
+++ test_scripts/test_sd.py
@@ -1,34 +1,16 @@
 import pandas as pd
 import src.experiments.ExperimentManager as u
 from src.experiments.model.ExperimentDataset import ExperimentDataset
+from src.experiments.config.DataUtils import map_target, clean, generate_names
 import time as t
 
 from src.subgroup_discovery.BI import BestInterval
 
 exp_man = u.ExperimentManager()
 
-def map_target(data: pd.DataFrame, y_col_name: str, to_ones):
-    data.loc[:, y_col_name] = data.loc[:, y_col_name].map(lambda e: 1 if e == to_ones else 0)
-    return data
-
-
-def clean(data: pd.DataFrame, value):
-    for row in range(data.shape[0]):
-        if data.loc[row,:].isin([value]).any():
-            data = data.drop(index=row)
-    return data
-
-
-def generate_names(number):
-    numbers = range(1,number + 1)
-    x_names = list(map(lambda num: "X" + str(num), numbers))
-    x_names.append("y")
-    return x_names
-
-
 gamma_telescope_names = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha",
                          "fDist", "class"]
-gamma_telescope = pd.read_csv("resources/data/gammatelescope/magic04.data", names=gamma_telescope_names)
+gamma_telescope = pd.read_csv("../resources/data/gammatelescope/magic04.data", names=gamma_telescope_names)
 gamma_telescope_yname = "class"
 gamma_telescope = gamma_telescope.dropna()
 gamma_telescope = map_target(gamma_telescope, gamma_telescope_yname, "g")
@@ -86,7 +68,7 @@ def generate_names(number):
     x_names.append("y")
     return x_names
 
-sylva = pd.read_csv("resources/data/sylva_prior.csv", header=0)
+sylva = pd.read_csv("../resources/data/sylva_prior.csv", header=0)
 sylva_yname = "label"
 sylva = sylva.dropna()
 sylva = sylva.drop(columns=["Rawah_Wilderness_Area","Neota_Wilderness_Area","Comanche_Peak_Wilderness_Area","Cache_la_Poudre_Wilderness_Area",
@@ -110,7 +92,7 @@ sylva800 = ExperimentDataset("sylva800", sylva, sylva_yname, fragment_size=800)
 sylva1600 = ExperimentDataset("sylva1600", sylva, sylva_yname, fragment_size=1600)
 sylva2400 = ExperimentDataset("sylva2400", sylva, sylva_yname, fragment_size=2400)
 
-avila = pd.read_csv("resources/data/avila/avila.txt", header=0, names=generate_names(10))
+avila = pd.read_csv("../resources/data/avila/avila.txt", header=0, names=generate_names(10))
 avila_yname = "y"
 avila = avila.dropna()
 avila = map_target(avila, avila_yname, "A")
