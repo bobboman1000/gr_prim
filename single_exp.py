5fa75207a22f1d15cf9469056c654be90800b243
Benjamin Jochum <uzebb@student.kit.edu>
2020-05-07 17:40:07 +0200

    update single_exp

--- single_exp.py
+++ single_exp.py
@@ -18,8 +18,8 @@ clean2 = clean2.drop(columns=["molecule_name", "conformation_name"])
 ex_data = ExperimentDataset("clean2", clean2, clean2_yname, fragment_size=400)
 
 
-exp_man.add_experiment(ex_data, DummyGenerator(), DummyMetaModel(), c.discovery_algs["prim"], name="dummy_dummy_prim_clean2", new_samples=2500, fragment_limit=1, enable_probabilities=True, scaling=ZERO_ONE_SCALING)
-exp_man.add_experiment(ex_data, c.generators["munge1"], c.metamodels["classRF"], c.discovery_algs["prim"], name="kde-si_classRF_prim_clean2", new_samples=2500, fragment_limit=1, enable_probabilities=True, scaling=Z_SCORE_SCALING)
+exp_man.add_experiment(ex_data, DummyGenerator(), DummyMetaModel(), c.discovery_algs["prim"], name="dummy_dummy_prim_clean2", new_samples=2500, fragment_limit=30, enable_probabilities=True, scaling=ZERO_ONE_SCALING)
+exp_man.add_experiment(ex_data, c.generators["munge1"], c.metamodels["classRF"], c.discovery_algs["prim"], name="kde-si_classRF_prim_clean2", new_samples=2500, fragment_limit=30, enable_probabilities=True, scaling=Z_SCORE_SCALING)
 
 exp_man.run_all_parallel(2)
 exp_man.export_experiments("clean2")
